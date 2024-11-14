3306df032e01812e103f640706d5e951f654959d
yakusoku_suru <okedelep@gmail.com>
2024-11-14 23:10:31 +0100

    lesson 17 - foreign keys(pt 2)

--- resources/views/ninjas/show.blade.php
+++ resources/views/ninjas/show.blade.php
@@ -6,6 +6,15 @@
         <p><strong>Skill level:</strong> {{ $ninja->skill }}</p>
         <p><strong>About me:</strong></p>
         <p>{{ $ninja->bio }}</p>
+
+        {{-- dojo info --}}
+        <div class="border-2 border-dashed bg-white px-4 pb-4 my-4 rounded">
+          <h3>Dojo Information</h3>
+          <p><strong>Dojo name:</strong> {{ $ninja->dojo->name }}</p>
+          <p><strong>Location:</strong> {{ $ninja->dojo->location }}</p>
+          <p><strong>About the Dojo:</strong></p>
+          <p>{{ $ninja->dojo->description }}</p>
+        </div>
     </div>
 
 </x-layout>
