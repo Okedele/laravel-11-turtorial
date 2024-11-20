1831dfb97607d0c9b61e3f3248f9a3f394723b02
yakusoku_suru <okedelep@gmail.com>
2024-11-20 20:14:46 +0100

    lesson 22 - flash messages

--- app/Http/Controllers/NinjaController.php
+++ app/Http/Controllers/NinjaController.php
@@ -40,7 +40,7 @@ class NinjaController extends Controller
 
       Ninja::create($validated);
 
-      return redirect()->route('ninjas.index');
+      return redirect()->route('ninjas.index')->with('success', 'Ninja created!');
 
       }
 
@@ -49,7 +49,7 @@ class NinjaController extends Controller
         $ninja = Ninja::findOrFail($id);
         $ninja->delete();
 
-        return redirect()->route('ninjas.index');
+        return redirect()->route('ninjas.index')->with('success', 'Ninja deleted!');
       }
 
       // edit() and update() for edit view and update requests
